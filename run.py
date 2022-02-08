a9cb37ce99fc7dd0871757c4584ec040b405e21d
bruzgus1 <poviliukas987412365@gmail.com>
2022-02-08 14:33:13 +0000

    changed function so it updates the tTitle ltle ledepending on user input

--- run.py
+++ run.py
@@ -17,11 +17,13 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('anime_watchlist')
 
-watchlist = SHEET.worksheet("watchlist").col_values(1)
+watchlist_col = SHEET.worksheet("watchlist").col_values(1)
 
-watchlist.pop(0)
+watchlist_col.pop(0)
 
-print(watchlist)
+print(watchlist_col)
+
+print()
 
 user_input = input("Anime Title you wish to add to the list: ")
 
@@ -31,9 +33,18 @@ def check_user_entry():
     """
     Checks if the Anime Title the user entered already exists in the worksheet
     """
-    for i in watchlist:
-        if i == user_input:
-            print("Error entry already exist")
+    if user_input in watchlist_col:
+        row_index = watchlist_col.index(user_input)+2
+        print()
+        question = input("Entry already exist, have you seen this Anime?, Please answer yes/no: ")
+        if question == "yes":
+            SHEET.worksheet("watchlist").update(f"B{row_index}", "yes")
+        elif question == "no":
+            SHEET.worksheet("watchlist").update(f"B{row_index}", "no")
+        else:
+            print("Wrong user input, Quiting program...")
+    else:
+        print("it activates every time the input does not match")
 
 
 check_user_entry()
